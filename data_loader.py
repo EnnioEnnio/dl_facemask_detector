1c466f367e2e3aff839e3eb61c9ad4600d11eb56
Ennio <ennio.strohauer@gmail.com>
2023-07-06 23:11:34 +0200

    Add description and observations for TODO methods

---
+++ data_loader.py
@@ -0,0 +1,11 @@
+"""
+TODO: data_loader.py
+
+This file is typically responsible for loading and preprocessing your data.
+It can handle tasks such as reading data from files, 
+performing data augmentation, data normalization, 
+and creating data loaders for efficient batching of the data during training and evaluation.
+
+Oberservations:
+Images from the dataset are of different sizes.
+"""
---
+++ run_model.py
@@ -0,0 +1,7 @@
+"""
+TODO: run_model.py
+
+The purpose of this file can vary depending on your specific use case.
+It can include code for running your trained model on new or unseen data and generating predictions or outputs.
+It can be used for deploying your model in a production environment or for using the model in a specific application.
+"""
