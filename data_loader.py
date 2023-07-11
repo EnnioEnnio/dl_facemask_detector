058709f9ae1fb34d97cb08a8474a16955d711c35
Samuel Kunst <samuel@kunst.me>
2023-07-11 17:19:58 +0200

    feat(dataloader): support zero-padding
    closes #7

--- data_loader.py
+++ data_loader.py
@@ -10,11 +10,19 @@ Obserservations:
 
 import os
 import argparse
-from PIL import Image
+from PIL import Image, ImageOps
 from pathlib import Path
 
 
-def resize_images(image_dir, output_dir, target_size=(256, 256), rotation=0):
+def apply_zero_padding(image, target_size):
+    new_image = Image.new(image.mode, target_size, 0)
+    new_image.paste(image, (0, 0))
+    return new_image
+
+
+def resize_images(
+    image_dir, output_dir, target_size=(256, 256), rotation=0, padding=False
+):
     """
     Resize images to target size and save them to output directory.
     Currently only supports .jpg and .png images.
@@ -26,7 +34,14 @@ def resize_images(image_dir, output_dir, target_size=(256, 256), rotation=0):
         if image_name.lower().endswith((".jpg", ".png")):
             image_path = os.path.join(image_dir, image_name)
             image = Image.open(image_path)
-            image = image.resize(target_size)
+            if padding:
+                # rescale to fit to target size (retains aspect ratio)
+                image = ImageOps.contain(
+                    image, target_size, method=Image.Resampling.LANCZOS
+                )
+                image = apply_zero_padding(image, target_size)
+            else:
+                image = image.resize(target_size)
 
             prefix = Path(image_name).with_suffix("")
             suffix = Path(image_name).suffix
@@ -37,13 +52,15 @@ def resize_images(image_dir, output_dir, target_size=(256, 256), rotation=0):
             image.save(output_path)
 
 
-def process_dataset(input_dir, output_dir, target_size, rotate):
+def process_dataset(input_dir, output_dir, target_size, rotate, padding):
     rotations = [90, 180, 270]
     for _, dir, _ in os.walk(input_dir):
         for d in dir:
             in_dir = os.path.abspath(os.path.join(input_dir, d))
             out_dir = os.path.abspath(os.path.join(output_dir, d))
-            resize_images(in_dir, out_dir, target_size=target_size)
+            # base image transformation
+            resize_images(in_dir, out_dir, target_size=target_size, padding=padding)
+            # optional, additional rotational variations
             if rotate:
                 for rotation in rotations:
                     resize_images(
@@ -51,15 +68,17 @@ def process_dataset(input_dir, output_dir, target_size, rotate):
                         out_dir,
                         target_size=target_size,
                         rotation=rotation,
+                        padding=padding,
                     )
 
 
 def main(args):
     process_dataset(
-        input_dir=args.input,
-        output_dir=args.output,
-        target_size=tuple(args.size),
-        rotate=args.rotate,
+        args.input,
+        args.output,
+        tuple(args.size),
+        args.rotate,
+        args.padding,
     )
 
 
@@ -89,5 +108,12 @@ if __name__ == "__main__":
         type=bool,
         action=argparse.BooleanOptionalAction,
         help="save additional rotated copies of source images (default: True)",
+    )
+    parser.add_argument(
+        "-p",
+        "--padding",
+        type=bool,
+        action=argparse.BooleanOptionalAction,
+        help="apply 0-padding to images to retain correct aspect ratio when resizing",
     )
     main(parser.parse_args())
