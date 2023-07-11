1bd99f57bada0a08ea9fa2c0a70a4912d4bb0b2a
Samuel Kunst <samuel@kunst.me>
2023-07-11 15:41:49 +0200

    data-loader: add script for manual data-processing
    The script will walk through all sub-directories of the passed
    input-dir. This is specifically modeled to work with the structure of
    the training dataset which we are using.

--- data_loader.py
+++ data_loader.py
@@ -1,16 +1,15 @@
 """
-TODO: data_loader.py
-
-This file is typically responsible for loading and preprocessing your data.
-It can handle tasks such as reading data from files, 
-performing data augmentation, data normalization, 
-and creating data loaders for efficient batching of the data during training and evaluation.
+This file is typically responsible for loading and preprocessing your data. It
+can handle tasks such as reading data from files, performing data augmentation,
+data normalization, and creating data loaders for efficient batching of the
+data during training and evaluation.
 
 Obserservations:
-Images from the dataset are of different sizes.
+    - Images from the dataset are of different sizes.
 """
 
 import os
+import argparse
 from PIL import Image
 
 
@@ -31,3 +30,38 @@ def resize_image(image_dir, output_dir, target_size=(256, 256)):
 
             output_path = os.path.join(output_dir, image_name)
             resized_image.save(output_path)
+
+
+def process_dataset(input_dir, output_dir, target_size):
+    for _, dir, _ in os.walk(input_dir):
+        for d in dir:
+            in_dir = os.path.abspath(os.path.join(input_dir, d))
+            out_dir = os.path.abspath(os.path.join(output_dir, d))
+            resize_image(in_dir, out_dir, target_size=target_size)
+
+
+def main(args):
+    process_dataset(args.input, args.output, tuple(args.size))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--input", type=str, required=True, help="input directory"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="./out",
+        help="output directory (default: ./out)",
+    )
+    parser.add_argument(
+        "-s",
+        "--size",
+        nargs=2,
+        type=int,
+        default=[256, 256],
+        help="transformed image dimensions (w/h) (default: (256, 256))",
+    )
+    main(parser.parse_args())
