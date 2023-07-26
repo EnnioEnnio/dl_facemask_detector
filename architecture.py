07bde0f11d4b136214307eb706544c44dfdfea54
Samuel Kunst <samuel@kunst.me>
2023-07-26 18:31:17 +0200

    fix: stagnant loss in training loop
    Played around a lot with different loss functions, balancing of
    datasets, and model architectures. These changes seemed to provide a
    decent result. At the very least, the loss does not seem to stagnate
    anymore. Ran a quick training run with these changes had the following
    stats for the final epoch: (note the model was trained only for 20
    epochs before stopping, the larger architecture made training ~3x
    slower on my machine):
    
    batch_loss: 0.00003376441600266844
    loss: 0.00001994716729747381
    validation_accuracy: 0.9982096354166666
    validation_loss: 0.0042258706406101965
    
    The small values could also be partially influenced by the chosen loss
    function. At least what I have found is that loss is not directly
    comparable from one function to the next, only relative to the function
    itself.
    
    It is worth trying to slim down the architecture somewhat while still
    keeping the same results.

--- architecture.py
+++ architecture.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 # Define the network architecture
@@ -8,57 +7,41 @@ import torch.nn as nn
 # The output is a 1-element vector with the probabilities of a person being masked 1 or not 0
 
 
-# Reshaping layer to transform the input from a 2D image to a 4D tensor
-class Reshape(nn.Module):
-    def forward(self, x):
-        return x.view(-1, 3, 256, 256)
-
-
 class LeNetty(nn.Module):
     """An Interpretation of the LeNet-5 model."""
 
-    def __init__(
-        self,
-        lr=0.1,
-        num_classes=1,
-        momentum=0.9,
-        weight_decay=5e-4,
-        batch_size=128,
-        epochs=200,
-    ):
+    def __init__(self):
         super(LeNetty, self).__init__()
-
-        self.features = nn.Sequential(
-            Reshape(),
-            nn.Conv2d(3, 6, kernel_size=5, padding=2),
-            # output size: 6x256x256
-            nn.Sigmoid(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
-            # output size: 6x128x128
-            nn.Conv2d(6, 16, kernel_size=5, stride=2),
-            # output size: 16x62x62
-            nn.Sigmoid(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
-            # output size: 16x31x31
-            nn.Conv2d(16, 32, kernel_size=5, stride=2),
-            # output size: 32x14x14
-            nn.Sigmoid(),
-            nn.AvgPool2d(kernel_size=2, stride=2)
-            # output size: 32x7x7
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 6, kernel_size=5, stride=1, padding=0),
+            nn.BatchNorm2d(6),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.ReLU(),
         )
-
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            # output size: 32*7*7=1568
-            nn.Linear(32 * 7 * 7, 120),
-            nn.Sigmoid(),
-            nn.Linear(120, 84),
-            nn.Sigmoid(),
-            nn.Linear(84, 1),
-            nn.Sigmoid(),
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.ReLU(),
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(16 * 61 * 61, 512),
+            nn.ReLU(),
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(),
+        )
+        self.fc3 = nn.Sequential(
+            nn.Linear(256, 1),
         )
 
     def forward(self, x):
-        x = self.features(x)
-        x = self.classifier(x)
-        return x
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return self.fc3(x)
--- train_model.py
+++ train_model.py
@@ -9,72 +9,23 @@ and performing early stopping if necessary.
 
 from architecture import LeNetty
 from torch import Tensor
+import gc
 from torch.utils.data import DataLoader, WeightedRandomSampler, random_split
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from tqdm import tqdm
-import configparser
 import logging as log
 import numpy as np
 import os
 import torch
 import wandb
 
+from config import Config
+
 log.basicConfig(level=log.INFO, format="[%(levelname)s] %(message)s")
 
 
-def train_model(
-    model,
-    data_set,
-    epochs=200,
-    batch_size=128,
-    learning_rate=0.1,
-    validation_split=0.2,
-    early_stopping_patience=10,
-    checkpointing=False,
-    loss_function=torch.nn.BCELoss,
-    optimizer=torch.optim.RMSprop,
-):
-    log.info(f"Validation split: {validation_split}")
-
-    training_set, validation_set = random_split(
-        data_set, [1 - validation_split, validation_split]
-    )
-
-    log.info(f"Training dataset: {len(training_set)} samples")
-    log.info(f"Validation dataset: {len(validation_set)} samples")
-    log.info(f"Detected classes: {data_set.class_to_idx}")
-
-    # creates a balanced sampler for subsets of a torch Dataset object (ex ImageFolder)
-    def balanced_sampler(set):
-        indices = set.indices
-        class_labels = [dataset.targets[i] for i in indices]
-        class_sample_count = np.bincount(class_labels)
-        class_weights = 1.0 / class_sample_count
-        sample_weights = np.array([class_weights[t] for t in class_labels])
-        return WeightedRandomSampler(
-            torch.from_numpy(sample_weights), len(sample_weights)
-        )
-
-    training_loader = DataLoader(
-        training_set,
-        batch_size=batch_size,
-        num_workers=4,
-        pin_memory=True,
-        drop_last=True,
-        sampler=balanced_sampler(training_set),
-    )
-
-    validation_loader = DataLoader(
-        training_set,
-        batch_size=batch_size,
-        num_workers=4,
-        pin_memory=True,
-        drop_last=True,
-        sampler=balanced_sampler(validation_set),
-    )
-
-    # init model, cuda, optimizer, and loss function
+def get_device():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     if device == "cuda":
         log.info(
@@ -82,10 +33,62 @@ def train_model(
         )
     else:
         log.info("Using computation device: cpu")
+    return device
 
+
+def make_balanced_dataloader(set, batch_size):
+    # creates a balanced sampler for subsets of a torch Dataset object (ex ImageFolder)
+    indices = set.indices
+    class_labels = [dataset.targets[i] for i in indices]
+    class_sample_count = np.bincount(class_labels)
+    class_weights = 1.0 / class_sample_count
+    sample_weights = np.array([class_weights[t] for t in class_labels])
+    sampler = WeightedRandomSampler(
+        torch.from_numpy(sample_weights), len(sample_weights)
+    )
+    return DataLoader(
+        set,
+        batch_size=batch_size,
+        num_workers=4,
+        pin_memory=True,
+        drop_last=True,
+        sampler=sampler,
+    )
+
+
+def train_model(
+    model,
+    data_set,
+    epochs=200,
+    batch_size=128,
+    learning_rate=0.01,
+    momentum=0.9,
+    validation_split=0.2,
+    early_stopping_patience=10,
+    checkpointing=False,
+    loss_function=torch.nn.BCEWithLogitsLoss,
+    optimizer=torch.optim.SGD,
+    save_model=True,
+):
+    training_set, validation_set = random_split(
+        data_set, [1 - validation_split, validation_split]
+    )
+    training_loader = make_balanced_dataloader(training_set, batch_size)
+    validation_loader = make_balanced_dataloader(validation_set, batch_size)
+    log.info(f"Validation split: {validation_split}")
+    log.info(f"Training dataset: {len(training_set)} samples")
+    log.info(f"Validation dataset: {len(validation_set)} samples")
+    log.info(f"Detected classes: {data_set.class_to_idx}")
+
+    # init model, cuda, optimizer, and loss function
+    device = get_device()
     log.debug(f"Model: {model}")
     neural_net = model.to(device)
-    optimizer = optimizer(neural_net.parameters(), lr=learning_rate)
+    optimizer = optimizer(
+        neural_net.parameters(),
+        lr=learning_rate,
+        momentum=momentum,
+    )
     loss = loss_function()
 
     # initialize weights and biases logging (wandb)
@@ -105,9 +108,6 @@ def train_model(
     best_loss = np.inf
     epochs_without_improvement = 0
     num_epochs = epochs
-    num_training_batches = len(training_loader)
-    num_validation_batches = len(validation_loader)
-
     # main training Loop
     epochs = tqdm(range(epochs), desc="Epoch progress", unit="epochs", total=epochs)
     for epoch in epochs:
@@ -118,51 +118,67 @@ def train_model(
             desc=f"Epoch {epoch_idx}: Training progress",
             unit="batches",
             leave=False,
-            total=num_training_batches,
+            total=len(training_loader),
         )
         for batch, (input, label) in training_batches:
+            torch.cuda.empty_cache()
+            optimizer.zero_grad()
+
             input = input.to(device)
             label = label.to(device).float()
-            output = neural_net(input).reshape(batch_size)
+            output = neural_net(input).squeeze()
 
             # backprop
-            current_batch_loss = loss(output, label)
-            optimizer.zero_grad()
-            current_batch_loss.backward()
+            batch_loss = loss(output, label)
+            batch_loss.backward()
             optimizer.step()
 
             # accumulate loss for the current epoch
-            epoch_loss += (current_batch_loss / num_training_batches).item()
+            epoch_loss += (batch_loss / len(training_loader)).item()
 
-            wandb.log({"batch_loss": current_batch_loss.item()})
+            wandb.log({"batch_loss": batch_loss.item()})
             tqdm.write(
                 f"Epoch {epoch_idx:03}/{num_epochs:03},"
                 f"Batch: {batch:4},"
-                f"Batch loss: {current_batch_loss:12f},"
+                f"Batch loss: {batch_loss:12f},"
                 f"Running epoch loss: {epoch_loss:12f},"
                 f"Split: {np.bincount(Tensor.cpu(label)) / batch_size}"
             )
 
-        # validation testing
-        tqdm.write(f"Epoch {epoch_idx:03}/{num_epochs:03}, performing validation")
-        validation_loss = 0.0
+            del input, label, batch_loss
+            gc.collect()
+
+        # validation loop
+        tqdm.write(f"Epoch {epoch+1:03}/{len(epochs):03}," f"Performing validation")
         validation_batches = tqdm(
-            enumerate(validation_loader),
-            desc=f"Epoch {epoch_idx}: Validation progress",
-            unit="batches",
-            leave=False,
-            total=num_validation_batches,
+            enumerate(validation_loader), total=len(validation_loader)
         )
-        for batch, (input, label) in validation_batches:
-            input = input.to(device)
-            label = label.to(device).float()
-            output = neural_net(input).reshape(batch_size)
-            current_batch_loss = loss(output, label)
-            validation_loss += (current_batch_loss / num_validation_batches).item()
+        with torch.no_grad():
+            validation_loss = 0.0
+            num_correct = 0
+            num_samples = len(validation_loader) * batch_size
+            for batch, (images, labels) in validation_batches:
+                labels = labels.to(device).float()
+                outputs = model(images.to(device)).squeeze()
+                batch_vloss = loss(outputs, labels)
+                validation_loss += (batch_vloss / len(validation_loader)).item()
+
+                predictions = torch.round(torch.sigmoid(outputs))
+                num_correct += torch.sum(predictions == labels).item()
+
+            validation_accuracy = num_correct / num_samples
+
         tqdm.write(
-            f"Epoch {epoch_idx:03}/{num_epochs:03}, Validation loss: {validation_loss:12f}"
+            f"Epoch {epoch_idx:03}/{num_epochs:03},"
+            f"Validation loss: {validation_loss:12f},"
+            f"Validation accuracy: {validation_accuracy:12f}"
         )
-        wandb.log({"validation_loss": validation_loss})
+        wandb.log(
+            {
+                "validation_loss": validation_loss,
+                "validation_accuracy": validation_accuracy,
+            }
+        )
 
         # end of current epoch, log current loss and perform checkpointing / early stopping if needed
         wandb.log({"loss": epoch_loss})
@@ -185,28 +201,31 @@ def train_model(
             tqdm.write(f"Early stopping after {epoch_idx} epochs without improvement")
             break
 
+    if save_model:
+        file = f"{model.__class__.__name__}-trained.pt"
+        tqdm.write(f"Saving model to {file}")
+        torch.save(neural_net.state_dict(), file)
+        wandb.save(file)
     # Close weights and biases logging (needed for Jupyter Notebooks)
     wandb.finish()
-    # Return the trained model
     return neural_net
 
 
 if __name__ == "__main__":
     model = LeNetty()
-
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-    config_value = config.get("Paths", "dataset", fallback=None)
-    dataset_path = os.getenv("DATASET_PATH") or config_value or "./dataset"
-
-    dataset_path = os.path.abspath(dataset_path)
+    config = Config()
+    dataset_path = os.path.abspath(
+        os.getenv("DATASET_PATH")
+        or config.get("Paths", "dataset")
+        or "./datasets/training_set"
+    )
     log.info(f"Dataset path: {dataset_path}")
 
     img_transform = transforms.Compose(
         [
-            transforms.Resize((256, 256)),
-            # transforms.RandomRotation(degrees=(90, 90)),
             transforms.ToTensor(),
+            transforms.Resize((256, 256), antialias=True),
+            transforms.Normalize((0.5,), (0.5,)),
         ]
     )
     dataset = ImageFolder(
@@ -215,5 +234,4 @@ if __name__ == "__main__":
     )
 
     model.train(True)
-    trained_model = train_model(model, dataset)
-    torch.save(trained_model.state_dict(), f"{model.__class__.__name__}-trained.pt")
+    train_model(model, dataset)
