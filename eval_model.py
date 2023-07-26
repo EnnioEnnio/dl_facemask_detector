da032c6833450ffa4c28d2391e65025a281adab2
Samuel Kunst <samuel@kunst.me>
2023-07-26 18:37:46 +0200

    feat: add basic model evaluation script

--- eval_model.py
+++ eval_model.py
@@ -1,7 +1,64 @@
-"""
-TODO: eval_model.py
+from architecture import LeNetty
+from config import Config
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+import torch
+import logging as log
+import os
 
-This file can be used for evaluating your trained model on a validation or test dataset.
-It can include code to load the trained model, load the evaluation data, perform inference using the model, 
-and compute evaluation metrics such as accuracy, precision, recall, etc.
-"""
+log.basicConfig(level=log.INFO, format="[%(levelname)s] %(message)s")
+
+
+def eval_model(model, test_set):
+    def get_class(idx):
+        return test_set.classes[idx]
+
+    log.info(f"test dataset: {len(test_set)} samples")
+    log.info(f"Detected classes: {test_set.class_to_idx}")
+    test_loader = DataLoader(test_set, shuffle=True)
+
+    num_correct = 0
+    total = len(test_set)
+    for data, label in test_loader:
+        actual = label.item()
+        prediction = 1 if model(data).item() > 0.5 else 0
+        print(
+            f"Actual: {actual} ({get_class(actual)}), Prediction: {prediction} ({get_class(prediction)})"
+        )
+        if actual == prediction:
+            num_correct += 1
+    print(f"Accuracy: {num_correct/total}")
+
+
+if __name__ == "__main__":
+    model = LeNetty()
+    config = Config()
+
+    # load pre-trained model
+    model_path = os.path.abspath(
+        os.getenv("MODEL") or config.get("Paths", "model") or "./trained.pt"
+    )
+
+    # load path to testing set
+    testset_path = os.path.abspath(
+        os.getenv("TESTSET_PATH")
+        or config.get("Paths", "testset")
+        or "./datasets/testing_set"
+    )
+    log.info(f"Test-set path: {testset_path}")
+
+    test_set = ImageFolder(
+        root=testset_path,
+        transform=transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Resize((256, 256), antialias=True),
+                transforms.Normalize((0.5,), (0.5,)),
+            ]
+        ),
+    )
+
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    eval_model(model, test_set)
